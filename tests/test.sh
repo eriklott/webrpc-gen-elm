ce9999ff822b323e38cd4ac804594cacbb1cc6cc
Erik Lott <mreriklott@gmail.com>
2023-04-01 00:52:29 -0400

    fix bugs and setup testing

--- tests/test.sh
+++ tests/test.sh
@@ -4,7 +4,7 @@ export PORT=9988
 
 webrpc-test -version
 webrpc-test -print-schema > ./test.ridl
-webrpc-gen -schema=./test.ridl -target=../ -out=./src/API.elm
+webrpc-gen -schema=./test.ridl -target=../ -out=./src/API.elm -baseurl=http://localhost:9988
 
 webrpc-test -server -port=$PORT -timeout=5s &
 
