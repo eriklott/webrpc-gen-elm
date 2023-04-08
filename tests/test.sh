cf0a3b8e56524dbee9a9132a30a839bdd4f89024
Erik Lott <mreriklott@gmail.com>
2023-04-08 00:59:07 -0400

    add generator for webrpc errors

--- tests/test.sh
+++ tests/test.sh
@@ -11,7 +11,8 @@ npx http-server -p 8080 -P http://localhost:9988 ./dist &
 webrpc-test -server -port=9988 -timeout=30s &
 
 # Wait until http://localhost:$PORT is available, up to 10s.
-for (( i=0; i<100; i++ )); do nc -z localhost $PORT && break || sleep 0.1; done
+for (( i=0; i<100; i++ )); do nc -z localhost 8080 && break || sleep 0.1; done
+for (( i=0; i<100; i++ )); do nc -z localhost 9988 && break || sleep 0.1; done
 
 # npm run elm-test
 npx cypress run
